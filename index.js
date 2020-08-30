4f6fb7a2b322f98a42d6320bd2bc46c0492f29ab
SonQH <sonqhb@gmail.com>
2020-08-30 16:32:27 +0700

    Add template engines Pug,and add method Post user, add package nodemon, add lowdb for project

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ users: [] })
+  .write()
+
+app.set('view engine', 'pug');
+app.set('views','./views')
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.render('index',{
+    name: 'Quach'
+  })
+})
+app.get('/users',(req, res) =>{
+  res.render('users/index',{
+    users: db.get('users').value()
+  })
+})
+app.get('/users/search',(req, res)=>{
+  var q = req.query.q;
+  var matchedUsers = users.filter(function(user){
+      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  })
+  console.log(req.query)
+  res.render('users/index',{
+    users: matchedUsers
+  });
 })
 
+app.get('/users/create', (req, res)=>{
+  res.render('users/create')
+})
+
+app.post('/users/create',(req, res)=>{
+  db.get('users').push(req.body).write();
+  console.log(req.body)
+  res.redirect('/users')
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
